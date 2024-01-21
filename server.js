41cb3ec51835098c747c2bc5235364383a6185a0
ubc-tuehoang <86985864+ubc-tuehoang@users.noreply.github.com>
2024-01-21 09:06:22 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let userGoal = 'My nwHack2024 Goal: NodeJS';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>Whatever nwHack2024 Goal!</h2>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+            <label>Project Objective</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Set Project Goal</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/store-goal', (req, res) => {
+  const enteredGoal = req.body.goal;
+  console.log(enteredGoal);
+  userGoal = enteredGoal;
+  res.redirect('/');
+});
+
+app.listen(8088);
